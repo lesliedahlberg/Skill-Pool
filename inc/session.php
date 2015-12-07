41d5760838d9030ebba6aa69430b5e9b7500ea37
lesliedahlberg <lesliedahlberg@253.68.243.130-v836.wireless.mdh.se>
2015-12-07 15:14:02 +0100

    sample login

---
+++ inc/session.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if($_SESSION["logged-in"] != "true"){
+  echo '<script type="text/javascript">window.location = "login.php"</script>';
+  echo 'JavaScript disabled, please click on the link to get redirected to your <a href="login.php">log in page!</a>';
+  die();
+  header.location('login.php');
+  exit();
+}
+
+?>
