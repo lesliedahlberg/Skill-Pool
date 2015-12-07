e26363c4ec7175eb655cf6b2b394324ac5a38564
lesliedahlberg <lesliedahlberg@Leslies-MacBook-Pro.local>
2015-12-07 15:55:31 +0100

    -||-

--- inc/session.php
+++ inc/session.php
@@ -3,7 +3,7 @@ session_start();
 /*if($_SESSION["logged-in"] != "true"){
   echo '<script type="text/javascript">window.location = "login.php"</script>';
   echo 'JavaScript disabled, please click on the link to get redirected to your <a href="login.php">log in page!</a>';
-  die();*/
-}
+  die();
+}*/
 
 ?>
