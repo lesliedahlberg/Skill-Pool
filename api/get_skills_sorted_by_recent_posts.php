c51badb688b17a68caa2a5e768b0ea466aa3ebd5
lesliedahlberg <lesliedahlberg@Leslies-MacBook-Pro.local>
2015-12-08 10:32:37 +0100

    Get skills sorted by recent posts

---
+++ api/get_skills_sorted_by_recent_posts.php
@@ -0,0 +1,28 @@
+<? //LESLIE ?>
+<?
+  //DB login
+  require_once '../lib/php/meekrodb.class.php';
+  require_once "../inc/db_credentials.php";
+
+  //Arrays
+  $errors = array();
+  $data = array();
+
+  //Get data from DB
+  $result = DB::query("
+    SELECT skill.id as id, skill.name as name, skill.category_id as category_id  FROM (SELECT skill_id, MAX(date_added) as date_added FROM skill_message GROUP BY skill_id) as t1, skill WHERE t1.skill_id = skill.id ORDER BY t1.date_added DESC
+  ");
+
+  //Set return statement
+  if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors']  = $errors;
+  } else {
+    $data['success'] = true;
+    $data['message'] = 'Skills retrieved!';
+    $data['result'] = $result;
+  }
+
+  //Return data
+  echo json_encode($data);
+?>
