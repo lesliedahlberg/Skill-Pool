01924c0ff3562a925c7cccd67ebf5df40274968b
jontelarsson94 <jonathan-larsson@outlook.com>
2015-12-08 16:44:14 +0100

    getskillsforuser

--- api/get_skills_for_user.php
+++ api/get_skills_for_user.php
@@ -1,1 +1,30 @@
 <? //JONATHAN ?>
+<?
+  //DB login
+  require_once '../lib/php/meekrodb.class.php';
+  require_once "../inc/db_credentials.php";
+  //Arrays
+  $errors = array();
+  $data = array();
+
+  //Arguments
+  if (!empty($_GET['user_id'])){
+    $user_id = $_GET['user_id'];
+  }
+  $user_id=1;
+    $result = DB::query("SELECT * from skill where id IN(Select skill_id from user_skill where user_skill.user_id=%i)", $user_id);
+
+
+  //Set return statement
+  if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors']  = $errors;
+  } else {
+    $data['success'] = true;
+    $data['message'] = 'Skills retrieved!';
+    $data['result'] = $result;
+  }
+
+  //Return data
+  echo json_encode($data);
+?>
