07b8419dad99051cc0b34c273a4eeeebe7e23812
lesliedahlberg <lesliedahlberg@Leslies-MacBook-Pro.local>
2015-12-08 18:34:47 +0100

    LOGIN fixes

--- inc/session_login_page.php
+++ inc/session_login_page.php
@@ -1,12 +1,12 @@
 <?
-
+session_start();
 
 if(isset($_SESSION))
 {
-  
+
     if($_SESSION['logged_in'] == true)
     {
-      header("Location: boards.php");
+      header("Location: board.php");
       die();
     }
 }
